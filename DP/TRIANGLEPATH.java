f0fb895758741c195b3573e5be242ea46f718fed
hwangjello <jmhwang21@naver.com>
2023-01-26 14:23:20 +0900

    종만북스 삼각형 경로 최댓값

--- DP/TRIANGLEPATH.java
+++ DP/TRIANGLEPATH.java
@@ -18,23 +18,30 @@ public class TRIANGLEPATH {
 		sum[0][0]=arr[0][0];
 		sum[1][0]=sum[0][0]+arr[1][0];
 		sum[1][1]=sum[0][0]+arr[1][1];
+        //처음 두 줄까지는 직접 입력
+
 		for(int k=2;k<n;k++) {
 			for(int l=0;l<k+1;l++) {
 				if(l==0) {
 					sum[k][l]=sum[k-1][l]+arr[k][l];
+                    //맨 왼쪽인 경우 위의 값만 받아오기
 				}
 				else {
 					sum[k][l]=Math.max(sum[k-1][l-1]+arr[k][l],sum[k-1][l]+arr[k][l]);
+                    //맨 왼쪽이 아닌경우 위의 값과 대각선 왼쪽값중 큰 값 받아 현재 값과 더하기
 				}
 			}
 		}
+
 		for(int a=0;a<n;a++) {
 			max=Math.max(max,sum[n-1][a]);
 		}
+        //변수 max에 sum 배열의 마지막 열을 돌며 최댓값 갱신
+
 		System.out.println(max);
 		
 		
-        
+
 	}
 
 }
